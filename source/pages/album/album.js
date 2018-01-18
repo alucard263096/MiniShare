acb5da592325aa940892edb8e53ccb840ca9d435
Steve <alucard263096@126.com>
2018-01-19 01:34:55 +0800

    完成删除相册

--- source/pages/album/album.js
+++ source/pages/album/album.js
@@ -1,11 +1,13 @@
 // pages/album/album.js
 import { AppBase } from "../../app/AppBase";
+import { AlbumApi } from "../../apis/album.api.js";
+
 class Content extends AppBase {
   constructor() {
     super();
   }
   onLoad(options) {
-    //options.id=5;
+    options.id=1;
     this.Base.Page = this;
     super.onLoad(options);
     this.Base.setMyData({
@@ -14,12 +16,21 @@ class Content extends AppBase {
       sliderOffset: 0,
       sliderLeft: 0,
       t1: 0,
-      t2: 0
+      t2: 0,
+      hiddenmodalput:true
     });
   }
   onShow() {
     var that = this;
     super.onShow();
+    var albumapi = new AlbumApi();
+    albumapi.list({ group_id: this.Base.options.id }, data => {
+      that.Base.setMyData({allalbum:data});
+    });
+    albumapi.list({ group_id: this.Base.options.id, createdmember_id: "0" }, data => {
+
+      that.Base.setMyData({ myalbum: data });
+    });
   }
 
   tabClick(e) {
@@ -28,10 +39,124 @@ class Content extends AppBase {
       activeIndex: e.currentTarget.id
     });
   }
+  inputAlbunname(e){
+    this.Base.setMyData({ albumname: e.detail.value});
+  }
+  createAlbum(){
+    this.Base.setMyData({ albumname: "", hiddenmodalput: false, album_id: "" });
+  }
+  cancelCreate(){
+    this.Base.setMyData({  hiddenmodalput: true });
+  }
+  confirmCreate() {
+    var data=this.Base.getMyData();
+    var albumname = data.albumname;
+    var album_id = data.album_id;
+    if(albumname.trim()==""){
+      this.Base.info("回忆主题不能为空");
+      return;
+    }
+    var that=this;
+    var albumapi = new AlbumApi();
+    console.log(album_id);
+    if(album_id!=""){
+
+      var postjson = { name: albumname, album_id: album_id };
+      console.log(postjson);
+      albumapi.rename(postjson, data => {
+        if (data.code == 0) {
+          that.onShow();
+        } else {
+          that.Base.error("系统正在维护，请稍后重试");
+        }
+      })
+    }else{
+
+      var postjson = { name: albumname, group_id: this.Base.options.id };
+      console.log(postjson);
+      that.Base.info("ca" + album_id);
+      return;
+      albumapi.create(postjson, data => {
+        if (data.code == 0) {
+          that.Base.info("创建成功");
+          that.onShow();
+        } else {
+          that.Base.error("系统正在维护，请稍后重试");
+        }
+      })
+    }
+    this.Base.setMyData({ hiddenmodalput: true });
+  }
+  changeAlbumname(e){
+    var name = e.currentTarget.dataset.name;
+    var sid = e.currentTarget.dataset.sid;
+    console.log(sid);
+    if(sid==""){
+
+      that.Base.info("ri");
+      return;
+    }
+    this.Base.setMyData({ albumname: name, album_id: sid, hiddenmodalput: false }); 
+  }
+  startManage(){
+    var data = this.Base.getMyData();
+
+    var allalbum=data.allalbum;
+    for(var i=0;i<allalbum.length;i++){
+      allalbum[i].selected=false;
+    }
+    this.Base.setMyData({ inmanage: true, allalbum: allalbum }); 
+  }
+  cancelManage(){
+    this.Base.setMyData({ inmanage: false }); 
+  }
+  selectAlbum(e){
+    var id=e.currentTarget.id;
+    
+    var data = this.Base.getMyData();
+    var allalbum = data.allalbum;
+    for (var i = 0; i < allalbum.length; i++) {
+      if (allalbum[i].id == id && allalbum[i].createdmember_id_name==AppBase.UserInfo.openid){
+        allalbum[i].selected = !allalbum[i].selected;
+      }
+    }
+    this.Base.setMyData({ inmanage: true, allalbum: allalbum }); 
+  }
+  deleteAlbum(){
+    var that=this;
+    var data = this.Base.getMyData();
+    var allalbum = data.allalbum;
+    var ids=[];
+    for (var i = 0; i < allalbum.length; i++) {
+      if (allalbum[i].selected == true && allalbum[i].createdmember_id_name == AppBase.UserInfo.openid) {
+        ids.push(allalbum[i].id);
+      }
+    }
+    ids=ids.join(",");
+    console.log(ids);
+    var albumapi = new AlbumApi();
+    albumapi.adelete({ idlist:ids},data=>{
+      if (data.code == 0) {
+        that.onShow();
+      } else {
+        that.Base.error("系统正在维护，请稍后重试");
+      }
+    });
+    this.Base.setMyData({ inmanage: false }); 
+  }
 }
 var page = new Content();
 var body = page.generateBodyJson();
 body.onLoad = page.onLoad;
-body.onShow = page.onShow;
-body.tabClick = page.tabClick;
+body.onShow = page.onShow; 
+body.tabClick = page.tabClick; 
+body.createAlbum = page.createAlbum;
+body.inputAlbunname = page.inputAlbunname;
+body.cancelCreate = page.cancelCreate; 
+body.confirmCreate = page.confirmCreate; 
+body.changeAlbumname = page.changeAlbumname;
+body.startManage = page.startManage; 
+body.cancelManage = page.cancelManage;
+body.selectAlbum = page.selectAlbum;
+body.deleteAlbum = page.deleteAlbum;
 Page(body)
